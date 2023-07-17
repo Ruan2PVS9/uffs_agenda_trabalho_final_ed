a2ac5309137c47b881ed218431000250696e767b
Ruan2PVS9 <ruan.souza.pv@gmail.com>
2023-07-16 23:58:33 -0300

    finalll

--- uffs_agenda.c
+++ uffs_agenda.c
@@ -1,39 +1,41 @@
-// -armazenar os eventos em uma estrutura de lista encadeada simples.
-// exempo de evento
-// Exemplo:
-//  Código: 1   (codigo pra que se não vai ser usado )
-//  Data: 24/12/2022
-//  Hora: 19:00
-//  Duração: 3.5
-//  Descrição: Jantar de Natal no Palácio do Alvorada
-// -A consulta deverá ser
-//       por data
-//       por data e hora
-// menu
-// 1-Incluir
-//      Verificar se já existe um compromisso agendado para a mesma data e horário.
-// 2-Consultar
-//      por data - todos no dia
-//      por data e hora -  o no dia e horario
-//      “Agenda Vazia” caso não existam
+// RUAN PABLO VITKOSKI DE SOUZA
+// 2221101033
+//  -armazenar os eventos em uma estrutura de lista encadeada simples.
+//  exempo de evento
+//  Exemplo:
+//   Código: 1   (codigo pra que se não vai ser usado )
+//   Data: 24/12/2022
+//   Hora: 19:00
+//   Duração: 3.5
+//   Descrição: Jantar de Natal no Palácio do Alvorada
+//  -A consulta deverá ser
+//        por data
+//        por data e hora
+//  menu
+//  1-Incluir
+//       Verificar se já existe um compromisso agendado para a mesma data e horário.
+//  2-Consultar
+//       por data - todos no dia
+//       por data e hora -  o no dia e horario
+//       “Agenda Vazia” caso não existam
 //
-// 3-Alterar
-//      alterar a descrição e a duração de um compromisso.
-//      alterar com base na data e hora (pra que usar codigo então ?)
-//      “Compromisso não encontrado” caso não exista compromisso para a data e hora
+//  3-Alterar
+//       alterar a descrição e a duração de um compromisso.
+//       alterar com base na data e hora (pra que usar codigo então ?)
+//       “Compromisso não encontrado” caso não exista compromisso para a data e hora
 //
-// 4-Excluir
-//      excluir comn consulta de data e hora
-// 5-Listar todos
-//      lista todos
-//          ordenados (opcional)
-// 6-Sair
-//      sair do programa
-//      limpar memoria
-// OPICIONAL
-//      BST (binary search tree)
-//      TAD (.h e .c)
-//      ordena os compromissos da agenda por data e hora.
+//  4-Excluir
+//       excluir comn consulta de data e hora
+//  5-Listar todos
+//       lista todos
+//           ordenados (opcional)
+//  6-Sair
+//       sair do programa
+//       limpar memoria
+//  OPICIONAL
+//       BST (binary search tree)
+//       TAD (.h e .c)
+//       ordena os compromissos da agenda por data e hora.
 
 #include <stdio.h>
 // #include <stdlib.h>
